728a7b4b8255f6d3f12d84960ee683c9346a14bd
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-11-20 15:04:39 -0600

    Finished project

--- sitebuilder/views.py
+++ sitebuilder/views.py
@@ -1,9 +1,11 @@
+import json
 import os
 
 from django.conf import settings
 from django.http import Http404
 from django.shortcuts import render
-from django.template import Template
+from django.template import Template, Context
+from django.template.loader_tags import BlockNode
 from django.utils._os import safe_join
 
 
@@ -16,10 +18,20 @@ def get_page_or_404(name):
     else:
         if not os.path.exists(file_path):
             raise Http404('Page Does Not Exist')
-
     with open(file_path, 'r') as f:
         page = Template(f.read())
-
+    """
+    This code loops through the page's raw nodelist and checks for a BlockNode
+    with the name context. BlockNode is a class definition for creating {% block %}
+    elements in Django templates. If the context BlockNode is found, it defines a
+    metavariable for us that contains the context (57).
+    """
+    meta = None
+    for i, node in enumerate(list(page.nodelist)):
+        if isinstance(node, BlockNode) and node.name == 'context':
+            meta = page.nodelist.pop(i)
+            break
+    page._meta = meta
     return page
 
 
@@ -31,4 +43,12 @@ def page(request, slug='index'):
         'slug': slug,
         'page': page,
     }
+    """
+    The metacontext is rendered using Python's json module to convert
+    {% block context %} into digestible Python (57).
+    """
+    if page._meta is not None:
+        meta = page._meta.render(Context())
+        extra_context = json.loads(meta)
+        context.update(extra_context)
     return render(request, 'page.html', context)
